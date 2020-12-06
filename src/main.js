255925dc29f8bf07d870a0b53bdfda7b6b1ffb1e
davidarroyo1234 <davidarroyo1234@gmail.com>
2020-12-06 23:56:58 +0100

    Function name miss spell fixed.

--- src/main.js
+++ src/main.js
@@ -14,80 +14,80 @@ let followersListClone;
 let scroll = setInterval(updateScroll, 1000);
 //starts checking if it is over.
 let stopCheck = setInterval(function () {
-    stopTask(1);
+	stopTask(1);
 }, 1000);
 
 /**
  * Checks if the loop needs to stop. Restarts if it is not over yet.
- * @param  {Number} number  "1" checks if the followedList is in same length as following title. 
+ * @param  {Number} number  "1" checks if the followedList is in same length as following title.
  *							"2" checks if the followersList is in same length as follower title.
  *
  */
 function stopTask(number) {
-    if (number === 1) {
+	if (number === 1) {
 		console.log("Verifying: " + parseInt(followedList.length) +"/" + followed +" followed people.");
-        if (followed <= parseInt(followedList.length) || followed - 1 <= parseInt(followedList.length)) {
-            clearInterval(scroll);
+		if (followed <= parseInt(followedList.length) || followed - 1 <= parseInt(followedList.length)) {
+			clearInterval(scroll);
 			console.log(" All donne. Starting to look for who follow you...");
-            followedList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
-            followedListClone = [...followedList];
-            followersF();
-        }
-    } else if (number === 2) {
+			followedList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
+			followedListClone = [...followedList];
+			followersF();
+		}
+	} else if (number === 2) {
 		console.log("Verifying: " + parseInt(followersList.length) +"/" + followers +" people who follow you.");
-        if (followers <= parseInt(followersList.length) || followers - 1 <= parseInt(followersList.length)) {
-            followersList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
-            followersListClone = [...followersList];
-            clearInterval(scroll);
+		if (followers <= parseInt(followersList.length) || followers - 1 <= parseInt(followersList.length)) {
+			followersList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
+			followersListClone = [...followersList];
+			clearInterval(scroll);
 			console.log(" All donne. Starting to look for who follow you back...");
-            users(1);
+			users(1);
 			clearInterval(stopCheck);
 			document.getElementsByClassName("-nal3 ")[2].click();
 			sleep(3000);
 			wantUnfollow = confirm("Do you want to unfollow this people we listed?");
 			if(wantUnfollow) users(2); else console.log("Thank You! All finished :)");
 			document.getElementsByClassName("wpO6b ")[1].click();
-        }
-    }
+		}
+	}
 }
 
 /**
  * Stops old check in followed and starts checking followers.
  */
 function followersF() {
-    clearInterval(stopCheck);
-    document.getElementsByClassName("-nal3 ")[1].click();
-    followers = followEDcount(1);
-    followersList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
-    scroll = setInterval(updateScroll, 1000);
-    stopCheck = setInterval(function () {
-        stopTask(2);
-    }, 1000);
+	clearInterval(stopCheck);
+	document.getElementsByClassName("-nal3 ")[1].click();
+	followers = followEDcount(1);
+	followersList = document.getElementsByClassName("FPmhX notranslate  _0imsa ");
+	scroll = setInterval(updateScroll, 1000);
+	stopCheck = setInterval(function () {
+		stopTask(2);
+	}, 1000);
 }
 
 
 /**
  * Prints the users who does not follow you back in the console.
  * @param {Number} option "1" for printing users.
-				   option "2" for gradual unfollowing users.
+ option "2" for gradual unfollowing users.
  */
 function users (option) {
-    let usernames = followersListClone.map(function (x) {
-        return x.title;
-    });
-	if (option == 1) {
+	let usernames = followersListClone.map(function (x) {
+		return x.title;
+	});
+	if (option === 1) {
 		for (let i = 0; i < followedListClone.length; i++) {
 			if (!usernames.includes(followedListClone[i].title)) {
 				console.log(followedListClone[i].title);
 			}
 		}
-	} else if(option == 2){
+	} else if(option === 2){
 		let counterStop = 0;
 		for (let i = 0; i < followedListClone.length; i++) {
 			if (!usernames.includes(followedListClone[i].title)) {
 				if (counterStop <= 30) {
 					console.log("Securing navigation before starting new unfollow: ");
-					sleepRandTime(5, 10);
+					sleepRandom(5, 10);
 					unfollowUser(followedListClone[i].title);
 					counterStop++;
 					console.log(counterStop + " unfollows.");
@@ -128,7 +128,7 @@ function unfollowUser(user) {
 	for (let i = 0; i < userPlace.length; i++) {
 		let nowUser = userPlace[i].title.toString().replace(/[,|.]/,"");
 		//console.log(nowUser + " === " + user);
-		if(nowUser == user) {
+		if(nowUser === user) {
 			// select right button and click it
 			buttonsOfDoc[i + 1].click();
 			// Confirm unfollow
@@ -137,7 +137,7 @@ function unfollowUser(user) {
 			break;
 		}
 	}
-	
+
 }
 
 /**
@@ -145,8 +145,8 @@ function unfollowUser(user) {
  */
 function updateScroll() {
 	//gets the div class element that holds the list and the scroll.
-    let element = document.getElementsByClassName("isgrP")[0];
-    element.scrollTop = element.scrollHeight;
+	let element = document.getElementsByClassName("isgrP")[0];
+	element.scrollTop = element.scrollHeight;
 }
 
 /**
@@ -154,11 +154,11 @@ function updateScroll() {
  * @param  {Number} milliseconds Number in milliseconds to sleep the code for a while.
  */
 function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
 }
 /**
  * Stop code random between secsMin and secsMax defined.
@@ -171,7 +171,7 @@ function sleepRandom (secsMin, secsMax) {
 		console.log("Sleeping for "+msToTime(sleepRandTime*1000)+".");
 		sleep(sleepRandTime*1000);
 	} else {
-		throw "Secs Min, cant be higher then secsMax"; 
+		throw "Secs Min, cant be higher then secsMax";
 	}
 }
 /**
@@ -180,15 +180,15 @@ function sleepRandom (secsMin, secsMax) {
  */
 function msToTime(miliseconds) {
 	let s = miliseconds;
-  var ms = s % 1000;
-  s = (s - ms) / 1000;
-  var secs = s % 60;
-  s = (s - secs) / 60;
-  var mins = s % 60;
-  var hrs = (s - mins) / 60;
+	var ms = s % 1000;
+	s = (s - ms) / 1000;
+	var secs = s % 60;
+	s = (s - secs) / 60;
+	var mins = s % 60;
+	var hrs = (s - mins) / 60;
 
-  //return hrs + ':' + mins + ':' + secs + '.' + ms;
-  return secs + ' secs';
+	//return hrs + ':' + mins + ':' + secs + '.' + ms;
+	return secs + ' secs';
 }
 
 /**
@@ -197,10 +197,10 @@ function msToTime(miliseconds) {
  * @return {Number} Returns the number of following or followed.
  */
 function followEDcount(num) {
-    if (document.getElementsByClassName("g47SY")[num].title==="") {
-        return parseInt(document.getElementsByClassName("g47SY")[num].innerText.toString().replace(/[,|.]/,""));
-    }
-    else {
-        return parseInt(document.getElementsByClassName("g47SY")[num].title.toString().replace(/[,|.]/,""));
-    }
+	if (document.getElementsByClassName("g47SY")[num].title==="") {
+		return parseInt(document.getElementsByClassName("g47SY")[num].innerText.toString().replace(/[,|.]/,""));
+	}
+	else {
+		return parseInt(document.getElementsByClassName("g47SY")[num].title.toString().replace(/[,|.]/,""));
+	}
 }
