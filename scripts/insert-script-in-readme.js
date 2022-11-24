4c3bc0f386f95ca02ce6ca0f6c031722cb7da8c9
davidarroyo1234 <29355377+davidarroyo1234@users.noreply.github.com>
2022-11-24 16:38:09 +0100

    fix comment

--- scripts/insert-script-in-readme.js
+++ scripts/insert-script-in-readme.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const readmePath = process.argv[2];
 const minifiedCodePath = process.argv[3];
 
-const codeBlockStart = "```js";
-const codeBlockEnd = "```";
+const CODE_BLOCK_START = "```js";
+const CODE_BLOCK_END = "```";
 
 const replaceRange = (s, start, end, substitute) => {
     return s.substring(0, start) + substitute + s.substring(end);
@@ -13,8 +13,8 @@ const replaceRange = (s, start, end, substitute) => {
 const readmeData = fs.readFileSync(readmePath, {encoding: 'utf8', flag: 'r'});
 const minifiedCode = fs.readFileSync(minifiedCodePath, {encoding: 'utf8', flag: 'r'});
 
-replaceStartIndex = readmeData.indexOf(codeBlockStart) + codeBlockStart.length + 1;
-replaceEndIndex = readmeData.lastIndexOf(codeBlockEnd) - 1;
+const replaceStartIndex = readmeData.indexOf(CODE_BLOCK_START) + CODE_BLOCK_START.length + 1;
+const replaceEndIndex = readmeData.lastIndexOf(CODE_BLOCK_END) - 1;
 
 const parsedReadme = replaceRange(readmeData, replaceStartIndex, replaceEndIndex, minifiedCode);
 fs.writeFileSync(readmePath, parsedReadme);
