ea82405e19366fbef46d365dd9676f14c769f382
davidarroyo1234 <29355377+davidarroyo1234@users.noreply.github.com>
2023-07-25 23:34:07 +0200

    Working script, pending fixing CODE_BLOCK_END

--- scripts/update-index.js
+++ scripts/update-index.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const indexPath = process.argv[2];
 const minifiedCodePath = process.argv[3];
 
-const CODE_BLOCK_START = "const instagramScript = '";
-const CODE_BLOCK_END = "'";
+const CODE_BLOCK_START = "const instagramScript = \"";
+const CODE_BLOCK_END = "\";";
 
 const replaceRange = (s, start, end, substitute) => {
-    return s.substring(0, start) + substitute + s.substring(end);
-}
+  return s.substring(0, start) + substitute + s.substring(end);
+};
 
-const indexData = fs.readFileSync(indexPath, {encoding: 'utf8', flag: 'r'});
-const minifiedCode = fs.readFileSync(minifiedCodePath, {encoding: 'utf8', flag: 'r'});
+const indexData = fs.readFileSync(indexPath, { encoding: "utf8", flag: "r" });
+let minifiedCode = fs.readFileSync(minifiedCodePath, { encoding: "utf8", flag: "r" });
 const replaceStartIndex = indexData.indexOf(CODE_BLOCK_START) + CODE_BLOCK_START.length;
-const replaceEndIndex = indexData.lastIndexOf(CODE_BLOCK_END) -1;
-
-
+const replaceEndIndex = indexData.lastIndexOf(CODE_BLOCK_END);
+minifiedCode = minifiedCode.replace(/"/g, "\\\"");
+minifiedCode = minifiedCode.replace(/\\n/g, "\\\\n");
 const parsedReadme = replaceRange(indexData, replaceStartIndex, replaceEndIndex, minifiedCode);
 fs.writeFileSync(indexPath, parsedReadme);
 
