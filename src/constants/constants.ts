4a32b17f41c430575c9465678052af7f58b8e57e
davidarroyo1234 <29355377+davidarroyo1234@users.noreply.github.com>
2024-08-04 13:50:45 +0200

    First component move

---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+export const INSTAGRAM_HOSTNAME = 'www.instagram.com';
+export const UNFOLLOWERS_PER_PAGE = 50;
+export const WHITELISTED_RESULTS_STORAGE_KEY = 'iu_whitelisted-results';
---
+++ src/utils/utils.ts
@@ -0,0 +1,24 @@
+import { UserNode } from "../model/user";
+import { UNFOLLOWERS_PER_PAGE } from "../constants/constants";
+
+export async function copyListToClipboard(nonFollowersList: readonly UserNode[]): Promise<void> {
+  const sortedList = [...nonFollowersList].sort((a, b) => (a.username > b.username ? 1 : -1));
+
+  let output = '';
+  sortedList.forEach(user => {
+    output += user.username + '\n';
+  });
+
+  await navigator.clipboard.writeText(output);
+  alert('List copied to clipboard!');
+}
+
+export function getMaxPage(nonFollowersList: readonly UserNode[]): number {
+  const pageCalc = Math.ceil(nonFollowersList.length / UNFOLLOWERS_PER_PAGE);
+  return pageCalc < 1 ? 1 : pageCalc;
+}
+
+export function getCurrentPageUnfollowers(nonFollowersList: readonly UserNode[], currentPage: number): readonly UserNode[] {
+  const sortedList = [...nonFollowersList].sort((a, b) => (a.username > b.username ? 1 : -1));
+  return sortedList.splice(UNFOLLOWERS_PER_PAGE * (currentPage - 1), UNFOLLOWERS_PER_PAGE);
+}
