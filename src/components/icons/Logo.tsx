69ee925830d47392c6469a797e8b659f0ca01afb
xyztavo <gustavobreasy@gmail.com>
2025-01-22 16:42:00 -0300

    fix: mobile responsive issues, not great but ok for now

---
+++ src/components/icons/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Logo = () => {
+    return (
+        <svg width="50" height="50" viewBox="0 0 354 354" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="177" cy="177" r="177" fill="#111111" />
+            <circle cx="177" cy="115" r="50" fill="#222222" />
+            <ellipse cx="177" cy="243" rx="76" ry="66" fill="#222222" />
+            <rect x="243" y="112" width="66" height="20" rx="10" fill="#00FFFF" />
+        </svg>
+    )
+}
